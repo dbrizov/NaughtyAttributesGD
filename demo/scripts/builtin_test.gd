1af51a6b031c2c975bfc602fa16c798cd8183de6
dbrizov <d.b.rizov@gmail.com>
2026-09-13 22:11:35 +0300

    Update builtin_test.gd and builtin_vs_naughty_test.gd

--- demo/scripts/builtin_test.gd
+++ demo/scripts/builtin_test.gd
@@ -2,4 +2,4 @@ extends Node
 
 @export var speed: float = 1.0
 @export_range(0, 10, 0.1) var slider: float = 5.0
-@export_multiline var mutiline: String = ""
+@export_custom(PROPERTY_HINT_FLAGS, "Fire,Ice,Wind") var flags: int = 0
--- demo/scripts/builtin_vs_naughty_test.gd
+++ demo/scripts/builtin_vs_naughty_test.gd
@@ -1,7 +1,18 @@
+@tool
 extends Node
 
-@export_range(0, 10, 0.1) var slider_builtin: float = 5.0
-@export_multiline var multiline_builtin: String = ""
+@export_group("Built-in")
+@export_range(0, 10, 0.1)
+var slider_builtin: float = 5.0
+@export_custom(PROPERTY_HINT_FLAGS, "Fire,Ice,Wind")
+var flags_builtin: int = 0
 
-@export_custom(PROPERTY_HINT_NONE, "range:0,10,0.1") var slider_naughty: float = 5.0
-@export_custom(PROPERTY_HINT_NONE, "multiline:") var multiline_naughty: String = ""
+@export_group("Naughty")
+@export_custom(PROPERTY_HINT_NONE, "range:0,10,0.1")
+var slider_naughty: float = 5.0
+@export_custom(PROPERTY_HINT_NONE, "flags:Fire,Ice,Wind; on_value_changed:print_flags")
+var flags_naughty: int = 0
+
+
+func print_flags(old_value, new_value):
+	print(old_value, " -> ", new_value)
