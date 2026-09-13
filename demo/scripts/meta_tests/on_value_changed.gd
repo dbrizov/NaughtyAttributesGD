773be77d3b3322199e3dba376ee65bed3bb58e72
dbrizov <d.b.rizov@gmail.com>
2026-09-13 17:49:37 +0300

    Modify the on_value_changed demo

--- demo/scripts/meta_tests/on_value_changed.gd
+++ demo/scripts/meta_tests/on_value_changed.gd
@@ -1,22 +1,14 @@
 @tool
 extends Node3D
 
-@export_custom(
-	PROPERTY_HINT_NONE, "on_value_changed:update_power; on_value_changed:_print_value_change"
-)
-var strength: int = 10
+@export_custom(PROPERTY_HINT_NONE, "on_value_changed:print_value_change") var strength: int = 10
 
-@export_custom(
-	PROPERTY_HINT_NONE, "on_value_changed:update_power; on_value_changed:_print_value_change"
-)
-var dexterity: int = 7
+@export_custom(PROPERTY_HINT_NONE, "on_value_changed:print_value_change") var dexterity: int = 7
 
-@export_custom(PROPERTY_HINT_NONE, "read_only") var power: int = 0
+@export_custom(PROPERTY_HINT_NONE, "read_only") var power: int:
+	get:
+		return strength * 2 + dexterity
 
 
-func update_power(_old_value, _new_value):
-	self.power = self.strength * 2 + self.dexterity
-
-
-func _print_value_change(old_value, new_value):
+func print_value_change(old_value, new_value):
 	print(old_value, " -> ", new_value)
